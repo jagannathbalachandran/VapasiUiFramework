823064dad952b40c121708c4112a1fd258f59661
Jaggu <jaggu@in-jaggu-3.local>
2019-04-04 14:07:32 +0530

    Adding BaseTestCase for set up and teardown

--- src/test/java/tests/BaseTestCase.java
+++ src/test/java/tests/BaseTestCase.java
@@ -1,20 +1,13 @@
 package tests;
 
-
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-
-public class SimpleLoginTest {
+public class BaseTestCase {
 
     ChromeDriver driver;
 
@@ -27,20 +20,9 @@ public class SimpleLoginTest {
         System.setProperty("webdriver.chrome.driver",currentUsersWorkingDir+"/src/test/resources/chromedriver");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        PageFactory.initElements(driver, this);
 
     }
-
-    @Test
-    public void testLogin(){
-        driver.navigate().to("https://spree-vapasi.herokuapp.com");
-        driver.navigate().to("https://spree-vapasi.herokuapp.com");
-        driver.findElement(By.id("link-to-login")).click();
-        driver.findElement(By.id("spree_user_email")).sendKeys("spree@example.com");
-        driver.findElement(By.id("spree_user_password")).sendKeys("spree123");
-        driver.findElement(By.name("commit")).click();
-    }
-
-
 
     @AfterTest
     public void tearDown(){
--- src/test/java/tests/SimpleLoginTestWithSnapshotOfTestFailure.java
+++ src/test/java/tests/SimpleLoginTestWithSnapshotOfTestFailure.java
@@ -3,10 +3,7 @@ package tests;
 
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -22,17 +19,17 @@ public class SimpleLoginTestWithSnapshotOfTestFailure {
 
     ChromeDriver driver;
 
-    @FindBy(id= "spree_user_emal")
-    WebElement user;
-
-    @FindBy(id="spree_user_password")
-    WebElement password;
-
-    @FindBy(name = "commit")
-    WebElement submit;
-
-    @FindBy(id = "link-to-login")
-    WebElement login_link;
+//    @FindBy(id= "spree_user_email")
+//    WebElement user;
+//
+//    @FindBy(id="spree_user_password")
+//    WebElement password;
+//
+//    @FindBy(name = "commit")
+//    WebElement submit;
+//
+//    @FindBy(id = "link-to-login")
+//    WebElement login_link;
 
     String currentUsersWorkingDir = System.getProperty("user.dir");
     private String resultsDir;
@@ -40,25 +37,21 @@ public class SimpleLoginTestWithSnapshotOfTestFailure {
     @BeforeTest
     public void setUp(ITestContext context){
 
-
         System.out.println("Dir is " + currentUsersWorkingDir);
         System.setProperty("webdriver.chrome.driver",currentUsersWorkingDir+"/src/test/resources/chromedriver");
         driver = new ChromeDriver();
         resultsDir = currentUsersWorkingDir+ "/src/test/snapshot";
-        context.setAttribute("driver", driver);
-        context.setAttribute("resultsDir", resultsDir);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        PageFactory.initElements(driver, this);
 
     }
 
     @Test
     public void testLogin(){
         driver.navigate().to("https://spree-vapasi.herokuapp.com");
-        login_link.click();
-        user.sendKeys("spree@example.com");
-        password.sendKeys("spree123");
-        submit.click();
+        driver.findElement(By.id("link-to-login")).click();
+        driver.findElement(By.id("spree_user_email")).sendKeys("spree@example.com");
+        driver.findElement(By.id("spree_user_password")).sendKeys("spree123");
+        driver.findElement(By.name("commit")).click();
     }
 
 
