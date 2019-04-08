05350e14103fb4781d5da62f83b5691c8335ca8e
Jaggu <jaggu@in-jaggu-3.local>
2019-04-08 16:19:32 +0530

    Renamed the test

--- src/test/java/tests/SimpleLoginTestWithSetUpAndTearDown.java
+++ src/test/java/tests/SimpleLoginTestWithSetUpAndTearDown.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 
-public class SimpleLoginTest {
+public class SimpleLoginTestWithSetUpAndTearDown {
 
     ChromeDriver driver;
 
